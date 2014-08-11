ce2a8f8577aa3c3d097eac1e31f7427e5545706c
Naoki Ikeno <nao@kyo-to.net>
2014-08-11 23:24:18 +0900

    Add a add.ctp file.

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -18,6 +18,18 @@ class PostsController extends AppController {
         }
         $this->set( 'post', $post );
     }
+
+    public function add(){
+        if( $this->request->is('post')){
+            $this->Post->create();
+            if( $this->Post-save( $this->request->data )){
+                $this->Session->setFlash( __('Your post has been saved.'));
+                return $this->redirect( array('action' => 'index' ));
+            }
+            $this->Session->setFlash( __('Unable to add your post.' ));
+        }
+    }
+
 }
 
 ?>
