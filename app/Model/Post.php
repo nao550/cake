f1db540b10c4a1e5aa647d8a4efab751ce444186
Naoki Ikeno <nao@kyo-to.net>
2014-08-11 23:28:49 +0900

    Move add.ctp file. and Completely Add post view.

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,10 +1,8 @@
 <?php
 class Post extends AppModel {
     public $validate = array( 
-        'title' => array( 'rule' => 'notEmpty' ),
-        'body' => array( 'rule' => 'notEmpty' )
-                              )
-        );
-
+           'title' => array( 'rule' => 'notEmpty' ),
+           'body' => array( 'rule' => 'notEmpty' )
+                              );
 }
 ?>
